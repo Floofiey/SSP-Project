b1cca0a98dc4ebb4c1369f2318e333846ecd6832
Gabriella Hawkes <ghawkes.dev@gmail.com>
2026-04-20 21:55:54 -0500

    update tests

--- Task3/run_task3_tests.py
+++ Task3/run_task3_tests.py
@@ -1,9 +1,10 @@
 from Task3.kubescape_pipeline import *
 
 
-test_detect_differences()
-test_map_to_kubescape_controls()
-test_run_kubescape_scan()
-test_export_to_csv()
+def run_task3():
+    test_detect_differences()
+    test_map_to_kubescape_controls()
+    test_run_kubescape_scan()
+    test_export_to_csv()
 
-print("Done!")
+    print("Done!")
--- run_all_tests.py
+++ run_all_tests.py
@@ -1,1 +1,3 @@
-import Task3.run_task3_tests
+from Task3.run_task3_tests import run_task3
+
+run_task3()
