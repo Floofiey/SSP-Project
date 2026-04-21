166a26b49c3b3f5b9a43f9ffb9ad7a4ee5f48593
Gabriella Hawkes <ghawkes.dev@gmail.com>
2026-04-20 22:34:53 -0500

    update req

--- test_cases.py
+++ test_cases.py
@@ -4,4 +4,5 @@ import Extractor
 sourcePath = os.path.dirname(os.path.realpath(__file__)) + "/SourcePDFs/"
 pdfoutPath = os.path.dirname(os.path.realpath(__file__)) + "/OutputPDFs/"
 
-def testPdfSplit()
+def testPdfSplit():
+    pass
