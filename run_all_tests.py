f7bdd96fdbc13fa3492c344f6508be4790e075ee
Gabriella Hawkes <ghawkes.dev@gmail.com>
2026-04-21 10:12:32 -0500

    try task 2 and 3 tests

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,7 +1,8 @@
 from Task3.run_task3_tests import run_task3
 from testComparator import run_test_cases as run_task2
 
-import Test
+# import Test
 
-# run_task3()
-# run_task2()
+# RUN TASK 1 TESTS HERE
+run_task3()
+run_task2()
